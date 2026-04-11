b7006c6f6278d6a3c2ffec241c08381a169ce83d
ZhangKunze <ZhangKunze@users.noreply.github.com>
2026-04-11 13:00:04 +0800

    把一个C程序的源码补上了(源码是我重写的，之前二进制的源码丢失了)

---
+++ 一个C程序.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+printf("Java or Bedrock?\n");
+sleep(2);
+printf("我说MC\n");
+sleep(2);
+printf("所以呢?\n");
+sleep(2);
+printf("Java还是基岩!\n回答我!\n");
+sleep(5);
+printf("好,你win了\n");
+return 0;
+}
