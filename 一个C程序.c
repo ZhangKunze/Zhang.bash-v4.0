eb7a8957b1b5be919370874c11ec9fec4797a99f
ZhangKunze <241098825+ZhangKunze@users.noreply.github.com>
2026-04-17 19:55:54 +0800

    更新 一个C程序.c
    更改为了有交互的

--- 一个C程序.c
+++ 一个C程序.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(){
-printf("Java or Bedrock?\n");
-sleep(2);
-printf("我说MC\n");
-sleep(2);
-printf("所以呢?\n");
-sleep(2);
-printf("Java还是基岩!\n回答我!\n");
-sleep(5);
-printf("好,你win了\n");
-return 0;
+    char name[9999];
+    
+    printf("Java or Bedrock?\n");
+    sleep(2);
+    printf("我说MC\n");
+    sleep(2);
+    printf("所以呢?\n");
+    sleep(2);
+    printf("Java还是基岩!\n回答我!\n");
+    
+    fgets(name, sizeof(name), stdin);
+    
+    printf("好,你win了\n");
+    return 0;
 }
